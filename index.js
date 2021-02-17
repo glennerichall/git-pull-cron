c8f936d779a2c7f2c80987ca8b5234091d7f16f7
Glenn Hall <glennerichall@hotmail.com>
2021-02-17 09:10:46 -0500

    made more verbose

--- index.js
+++ index.js
@@ -18,8 +18,14 @@ credentials = path.resolve(credentials);
 let configs = {};
 
 if (fs.existsSync(credentials)) {
-    configs = fs.readFileSync(credentials);
-    configs = JSON.parse(configs);
+    try {
+        configs = fs.readFileSync(credentials);
+        configs = JSON.parse(configs);
+    } catch (e) {
+        console.error('[failure] error loading config files')
+        console.error(e.message);
+        process.exit(1);
+    }
 }
 
 const token = process.env['GIT-BACKUP-TOKEN'] || argv.token || argv.t || configs.token;
@@ -28,21 +34,10 @@ const folder = process.env['GIT-BACKUP-DESTINATION'] || argv.destination || argv
 const login = process.env['GIT-BACKUP-LOGIN'] || argv.login || argv.l || configs.login;
 
 const logfile = process.env['GIT-BACKUP-LOGFILE'] || path.join(folder, 'git-backup.log');
-const api = process.env['GIT-BACKUP-API'] || argv.api || configs.api || 'github';
+const api = (process.env['GIT-BACKUP-API'] || argv.api || configs.api || 'github').toLowerCase();
 
-const getRepos = require(`./${api}`);
 
 (async function () {
-
-    if (!server) {
-        console.error('[failure] server must be specified using environment variable GIT-BACKUP-SERVER or program argument -s');
-        return;
-    }
-    if (!token) {
-        console.error('[failure] private token must be specified using environment variable GIT-BACKUP-TOKEN or program argument -t');
-        return;
-    }
-
     try {
         await mkdirp(folder);
         const toLogFile = stream({file: logfile, size: '100k', keep: 5});
@@ -57,11 +52,37 @@ const getRepos = require(`./${api}`);
             console.log(s);
         };
     } catch (err) {
-        console.log(`[failure] ${err.message}`);
+        console.log(`[failure] ${err.trace}`);
     }
 
+    let getRepos;
+    if (api === 'github') {
+        getRepos = require('./github');
+    } else if (api === 'gitlab') {
+        getRepos = require('./gitlab');
+    } else {
+        console.log(`[failure] unknown git provider: ${api}`)
+    }
+
+    if (!server) {
+        console.log('[failure] server must be specified using environment variable GIT-BACKUP-SERVER or program argument -s');
+        return;
+    }
+    if (!token) {
+        console.log('[failure] private token must be specified using environment variable GIT-BACKUP-TOKEN or program argument -t');
+        return;
+    }
+
+    console.log('[info] backup process started');
+    console.log(`[info] server:      ${server}`);
+    console.log(`[info] api:         ${api}`);
+    console.log(`[info] token:       *************`);
+    console.log(`[info] login:       *************`);
+    console.log(`[info] logfile:     ${logfile}`);
+    console.log(`[info] destination: ${folder}`);
 
     if (argv.cron) {
+        console.log('[info] creating cron job');
 
         const os = require('os').platform();
 
@@ -72,13 +93,13 @@ const getRepos = require(`./${api}`);
             .map(arg => `"${arg}"`)
             .join(' ');
 
-        if(fs.existsSync(credentials)){
+        if (fs.existsSync(credentials)) {
             args += ` --credentials="${credentials}"`;
         }
 
-        if (os === 'win32' ) {
+        if (os === 'win32') {
             await require('./win32')(args);
-        } else if(os === 'linux') {
+        } else if (os === 'linux') {
             await require('./linux')(args);
         } else {
             console.log('[failure] only linux, Macos and Windows are supported');
@@ -93,15 +114,30 @@ const getRepos = require(`./${api}`);
             .replace('https://', '')
             .replace(':', '_'));
 
-        await mkdirp(workingDir);
+        try {
+            await mkdirp(workingDir);
+        } catch (e) {
+            console.log(`[failure] error while creating directories in ${workingDir} \n ${e.trace}`);
+            process.exit(1);
+        }
 
-        let repos = await getRepos({server, token, login});
+        let repos;
+        try {
+            console.log(`[info] fetching repos from ${api} at ${server}`);
+            repos = await getRepos({server, token, login});
+        } catch (e) {
+            console.log(`[failure] error while getting repos from ${api} \n ${e.trace}`);
+        }
 
         const git = simpleGit();
 
-        console.log(`${repos.length} projects found`);
+        console.log(`[info] ${repos.length} projects found`);
 
-        console.log(`backuping git remote to ${workingDir}`);
+        if (!argv.dryrun) {
+            console.log(`[info] backuping git remote to ${workingDir}`);
+        } else {
+            console.log(`[info] not backuping git remote (dry-run)`);
+        }
         for (let i = 0; i < repos.length && !argv.dryrun; i++) {
 
             const project = repos[i];
@@ -136,7 +172,7 @@ const getRepos = require(`./${api}`);
             promises.push(promise);
         }
     } catch (err) {
-        console.log(`[failure] ${err.message}`);
+        console.log(`[failure] \n ${err.trace}`);
         success = false;
     } finally {
         await Promise.all(promises);
